0684eb4601c5cf9ff90c790b334ae442b2b1cc5a
alejandro <alejandro.aleman.hernandez@gmail.com>
2016-12-18 20:33:41 -0600

    dynamic links

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-(function () {
-	document.querySelector('#img').src = src[Math.floor(Math.random() * src.length)];
-})();
+// random image
+document.querySelector('#img').src = src[Math.floor(Math.random() * src.length)];
+
+// links list
+for (let i = 0; i < links.length / 3; i++) {
+	const linksList = document.createElement('div');
+	linksList.classList.toggle('links-list');
+	document.querySelector('#links').appendChild(linksList);
+}
+const linksLists = document.querySelectorAll('.links-list');
+for (let i = 0; i < links.length; i++) {
+	const linksListItem = document.createElement('a');
+	linksListItem.classList.toggle('links-list-item');
+	linksListItem.href = links[i].href;
+	linksListItem.innerHTML = links[i].label;
+	linksLists[Math.floor(i / 3)].appendChild(linksListItem);
+}
 
 // updateClock by tayumpee
 function updateClock() {
-	var days = ['DOMINGO', 'LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO'];
-	var months = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'];
-	var date = new Date();
-	var dateString =
+	const days = ['DOMINGO', 'LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO'];
+	const months = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'];
+	const date = new Date();
+	const dateString =
     (days[date.getDay()]) + ' ' +
     ('0' + date.getDate()).slice(-2) + ' DE ' +
     (months[date.getMonth()]) + ' DE ' +
@@ -22,13 +36,13 @@ function updateClock() {
 updateClock();
 
 document.getElementById('form').onsubmit = function () {
-	var search = document.getElementById('search');
-	var query = search.value;
+	const search = document.getElementById('search');
+	const query = search.value;
 	if (query.charAt(0) !== '-' || query.charAt(2) !== ' ') {
 		this.setAttribute('action', 'https://www.google.com.mx/search?q=search');
 		return true;
 	}
-	var command = query.charAt(1);
+	const command = query.charAt(1);
 	query = query.substring(3, query.length);
 	if (command === 'c') {
 		window.location.assign('https://' + query + '.com/');
